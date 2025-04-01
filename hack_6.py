accfc0cc34f9c4186982863d2d82d8cef170b220
dubraska <solorzano.dubraska@gmail.com>
2025-03-31 22:42:09 -0400

    hack 6 python

--- hack_6.py
+++ hack_6.py
@@ -4,5 +4,11 @@ loop: for [] output => [0,1,2,3,4,5]
 
 def fn_hack_6():
     result = []
-    #...
+    n=0
+    while (n < 6):
+        result.append(n)
+        n += 1
     return result  
+
+#Prueba interna
+print(fn_hack_6())
