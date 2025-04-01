ed3d7a6b2a30be6f899ad90cb8b5329886be16d2
dubraska <solorzano.dubraska@gmail.com>
2025-03-31 22:31:25 -0400

    hack 4 python

--- hack_4.py
+++ hack_4.py
@@ -4,5 +4,9 @@ text: "fooziman" output => "foozimaN"
 
 def fn_hack_4():
     result = "fooziman"
-    #...
+    #Separando elstr en "rebanadas" para manipular solo la parte que queremos
+    result = result[:-1] + result[-1].upper()
     return result
+
+#Prueba interna
+print(fn_hack_4())
