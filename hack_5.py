95741773be02a4fe36d7b9f69ec8ffde666b2046
dubraska <solorzano.dubraska@gmail.com>
2025-03-31 22:36:58 -0400

    hack 5 python

--- hack_5.py
+++ hack_5.py
@@ -4,5 +4,9 @@ text: "fooziman" output => "f00z1m@n"
 
 def fn_hack_5():
     result = "fooziman"
-    #...
+    #Reemplazando:
+    result = result.replace(result,"f00z1m@n")
     return result  
+
+#Prueba interna
+print(fn_hack_5())
