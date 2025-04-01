73b25c019e8ddc0827d9a6dea6f78de91ab0a5c0
dubraska <solorzano.dubraska@gmail.com>
2025-03-31 22:24:36 -0400

    hack 3 python

--- hack_3.py
+++ hack_3.py
@@ -4,5 +4,8 @@ text: "fooziman" output => "Fooziman"
 
 def fn_hack_3():
     result = "fooziman"
-    #...
+    result = result.capitalize()
     return result
+
+#Prueba interna
+print(fn_hack_3())
