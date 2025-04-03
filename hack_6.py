f33835a70a386b6c514f6a2534f80f2c4ab2ca49
dubraska <solorzano.dubraska@gmail.com>
2025-04-02 21:14:52 -0400

    hack 8 python + arreglando hack 6 con FOR

--- hack_6.py
+++ hack_6.py
@@ -4,10 +4,8 @@ loop: for [] output => [0,1,2,3,4,5]
 
 def fn_hack_6():
     result = []
-    n=0
-    while (n < 6):
+    for n in range(6):
         result.append(n)
-        n += 1
     return result  
 
 #Prueba interna
